e689eb9dd88b9faadb338f873e08215f410d1555
kondoumh <kondoumh@gmail.com>
2020-08-10 19:06:44 +0900

    Add env input

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,18 @@
 
 set -e
 
-sh -c "pack build ${INPUT_IMAGE}:${INPUT_TAG} --path ${INPUT_PATH} --builder ${INPUT_BUILDER}"
+env_str=""
+if [ -n "$INPUT_ENV" ]; then
+  read -r -a arr <<< "$INPUT_ENV"
+
+  for e in "${arr[@]}"
+  do
+    env_str+=" --env "
+    env_str+='"'$e'"'
+  done
+fi
+
+command="pack build ${INPUT_IMAGE}:${INPUT_TAG} ${env_str} --path ${INPUT_PATH} --builder ${INPUT_BUILDER}"
+echo "::set-output name=command::$command"
+
+sh -c $command
