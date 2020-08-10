2b336d4109c42f4a359f579be4da8b633617f68c
kondoumh <kondoumh@gmail.com>
2020-08-10 21:28:39 +0900

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,10 @@ if [ -n "$INPUT_ENV" ]; then
 
   for e in "${arr[@]}"
   do
-    env_str+=" --env $e"
+    env_str+=" --env "
+    env_str*='"'$e'"'
   done
 fi
+echo ${env_str}
 
-command="'"'pack build ${INPUT_IMAGE}:${INPUT_TAG} ${env_str} --path ${INPUT_PATH} --builder ${INPUT_BUILDER}'"'"
-echo $command
-
-echo "::set-output name=command::$command"
-
-sh -c $command
+sh -c "pack build ${INPUT_IMAGE}:${INPUT_TAG} ${env_str} --path ${INPUT_PATH} --builder ${INPUT_BUILDER}"
