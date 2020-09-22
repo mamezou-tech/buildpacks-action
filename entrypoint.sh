62a5f1cb41e5c6bada1cdac00cb159c7b1adc8f1
Masahiro Kondou <kondoh@mamezou.com>
2020-09-22 18:33:29 +0900

    Feature custom buildpack (#6)
    * add input for buildpacks
    
    * add job for buildpacks
    
    * fix sh
    
    * fix sh
    
    * update buildpacks to v0.13.1
    
    * update README.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,24 @@ set -e
 env_str=""
 if [ -n "$INPUT_ENV" ]; then
   arr=(${INPUT_ENV})
-
   for e in "${arr[@]}"
   do
     env_str+=" --env "
     env_str+='"'$e'"'
   done
 fi
-command="pack build ${INPUT_IMAGE}:${INPUT_TAG} ${env_str} --path ${INPUT_PATH} --builder ${INPUT_BUILDER}"
+
+buildpacks=""
+if [ -n "$INPUT_BUILDPACKS" ]; then
+  arr=(${INPUT_BUILDPACKS})
+  for p in "${arr[@]}"
+  do
+    buildpacks+=" --buildpack "
+    buildpacks+='"'$p'"'
+  done
+fi
+
+command="pack build ${INPUT_IMAGE}:${INPUT_TAG} ${env_str} --path ${INPUT_PATH} ${buildpacks} --builder ${INPUT_BUILDER}"
 echo "::set-output name=command::${command}"
 
 sh -c "${command}"
